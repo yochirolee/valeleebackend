4c441c855fd69af053673614b600f168b6dee114
Yochiro lee <yleecruz@gmail.com>
2025-08-09 14:07:07 -0400

    Add .gitignore

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
+// db.js
 const { Pool } = require('pg')
-require('dotenv').config()
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.POSTGRES_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
 })
 
 module.exports = { pool }
